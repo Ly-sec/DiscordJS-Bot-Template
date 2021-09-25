f5a55802eb64803cec9ccd2efc3931dcb611b143
Lysec <52084453+Ly-sec@users.noreply.github.com>
2021-09-25 11:08:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,7 @@ const { eventLoader } = require("./handler/events");
 const { commandLoader } = require("./handler/commands");
 
 const bot = new Client({
-  intents: [
-    "GUILDS",
-    "GUILD_BANS",
-    "GUILD_EMOJIS_AND_STICKERS",
-    "GUILD_INTEGRATIONS",
-    "GUILD_INVITES",
-    "GUILD_MEMBERS",
-    "GUILD_MESSAGES",
-    "GUILD_MESSAGE_REACTIONS",
-    "GUILD_MESSAGE_TYPING",
-    "GUILD_PRESENCES",
-    "GUILD_VOICE_STATES",
-    "GUILD_WEBHOOKS",
-    "DIRECT_MESSAGES",
-    "DIRECT_MESSAGE_REACTIONS",
-    "DIRECT_MESSAGE_TYPING",
-  ],
+  intents: [32767]
 });
 
 bot.commands = new Collection();
